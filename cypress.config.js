4ca013e51f7cc43d4b9d0fe6c400198d66dc8d9a
“Rajat <rajatvermaa95@gmail.com>
2023-11-15 00:12:39 +0530

    Login Test

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,18 @@
+/**
+#  * Author: Rajat Verma
+#  * Email: rajatvermaa95@gmail.com
+#  * GitHub Profile: https://github.com/rajatt95
+#  * GitHub Page: https://rajatt95.github.io/
+#  * LinkedIn: https://www.linkedin.com/in/rajatt95/
+#  * Topmate: https://topmate.io/rajatt95
+#  * Telegram: https://t.me/rajatt95
+#  * Instagram: https://www.instagram.com/rajattvermaa95/
+#  * YouTube: https://www.youtube.com/@rajatt95
+#  * WhatsApp: https://chat.whatsapp.com/DVRP0ViD0YwELUKF2XMJHy
+#  */
+
+/***************************************************/
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
---
+++ cypress/e2e/Sauce_Demo/Login.cy.js
@@ -0,0 +1,43 @@
+/**
+#  * Author: Rajat Verma
+#  * Email: rajatvermaa95@gmail.com
+#  * GitHub Profile: https://github.com/rajatt95
+#  * GitHub Page: https://rajatt95.github.io/
+#  * LinkedIn: https://www.linkedin.com/in/rajatt95/
+#  * Topmate: https://topmate.io/rajatt95
+#  * Telegram: https://t.me/rajatt95
+#  * Instagram: https://www.instagram.com/rajattvermaa95/
+#  * YouTube: https://www.youtube.com/@rajatt95
+#  * WhatsApp: https://chat.whatsapp.com/DVRP0ViD0YwELUKF2XMJHy
+#  */
+
+/***************************************************/
+
+/// <reference types="cypress" />
+
+describe('Sauce Demo - [LOGIN]', () => {
+  
+  beforeEach(() => {
+    // Navigate to the Sauce Demo website before each test
+    cy.visit('https://www.saucedemo.com/')
+  })
+
+  it('[LOGIN] Login with valid credentials. Validate that User is able to login using valid credentials.', () => {
+    
+    // Type valid credentials and login
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    // Assertions for an successful login 
+
+    // Verify successful login on the Products Page
+    cy.get('.title').should('have.text', 'Products')
+
+    // Verify common components on the page
+    cy.get('.app_logo').should('have.text', 'Swag Labs')
+    cy.get('.footer_copy').should('include.text', ' Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy')
+    
+  })
+  
+})
